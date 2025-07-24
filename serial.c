9a54dc6b924e69e619c1e2eb16943fdc5e3ffb46
Claudio Guarrasi <claudio.guarrasi01@universitadipavia.it>
2025-07-24 15:29:40 +0200

    refactor: improve performance by removing vectorial ops functions

--- serial.c
+++ serial.c
@@ -10,34 +10,12 @@ typedef struct{
 int bodies = 10;
 double GravConstant = 39.47;
 double dt = 0.01;
-int N = 5000; // number of iterations
+int N = 100; // number of iterations
 double *masses;
 vector *positions, *velocities, *accelerations;
 
-vector addVectors(vector a,vector b){
-	vector c = {a.x+b.x,a.y+b.y,a.z+b.z};
-
-	return c;
-}
-
-vector scaleVector(double b,vector a){
-	vector c = {b*a.x,b*a.y,b*a.z};
-
-	return c;
-}
-
-vector subtractVectors(vector a,vector b){
-	vector c = {a.x-b.x,a.y-b.y,a.z-b.z};
-
-	return c;
-}
-
-double mod(vector a){
-	return sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
-}
-
 double rand_uniform(unsigned int *seed, double min, double max) {
-    return min + (max - min) * ((double)rand_r(seed) / RAND_MAX);
+    return min + (max - min) * ((double)rand_r(seed) / RAND_MAX); //5+16
 }
 
 void initiateSystem(){
@@ -48,57 +26,76 @@ void initiateSystem(){
 	velocities = (vector*)malloc(bodies*sizeof(vector));
 	accelerations = (vector*)malloc(bodies*sizeof(vector));
 	
-	for (int i = 0; i < bodies; i++) {
-          unsigned int seed = i; // Deterministic seed per body
+	for (int i = 0; i < bodies; i++) { //3*bodies
+          unsigned int seed = i; // 1*bodies
 
-          positions[i].x = rand_uniform(&seed, -1.0, 1.0);
-          positions[i].y = rand_uniform(&seed, -1.0, 1.0);
-          positions[i].z = rand_uniform(&seed, -1.0, 1.0);
+          positions[i].x = rand_uniform(&seed, -1.0, 1.0); // (16+21+3+3)*bodies
+          positions[i].y = rand_uniform(&seed, -1.0, 1.0); // (16+21+3+3)*bodies
+          positions[i].z = rand_uniform(&seed, -1.0, 1.0); // (16+21+3+3)*bodies
 
-          velocities[i].x = rand_uniform(&seed, -0.5, 0.5);
-          velocities[i].y = rand_uniform(&seed, -0.5, 0.5);
-          velocities[i].z = rand_uniform(&seed, -0.5, 0.5);
+          velocities[i].x = rand_uniform(&seed, -0.5, 0.5);// (16+21+3+3)*bodies
+          velocities[i].y = rand_uniform(&seed, -0.5, 0.5);// (16+21+3+3)*bodies
+          velocities[i].z = rand_uniform(&seed, -0.5, 0.5);// (16+21+3+3)*bodies
 
-          masses[i] = 1.0; // For simplicity
+          masses[i] = 1.0; // For simplicity //(3+3)*bodies
 
           // Initialize accelerations to zero
-          accelerations[i].x = 0;
-          accelerations[i].y = 0;
-          accelerations[i].z = 0;
+          accelerations[i].x = 0; //(3+3)*bodies
+          accelerations[i].y = 0; //(3+3)*bodies
+          accelerations[i].z = 0; //(3+3)*bodies
         }
 }
 
-void computeAccelerations(){
-	int i,j;
-        const double epsilon = 1e-5;
-	for(i=0;i<bodies;i++){
-		accelerations[i].x = 0;
-		accelerations[i].y = 0;
-		accelerations[i].z = 0;
-		for(j=0;j<bodies;j++){
-			if(i!=j){
-			        double dist = mod(subtractVectors(positions[i], positions[j])) + epsilon;
-				accelerations[i] = addVectors(accelerations[i],scaleVector(GravConstant*masses[j]/pow(dist,3), subtractVectors(positions[j],positions[i])));
-			}
-		}
-	}
+void computeAccelerations() {
+    const double epsilon = 1e-5;
+    
+    for (int i = 0; i < bodies; i++) {
+        accelerations[i].x = 0.0;
+        accelerations[i].y = 0.0;
+        accelerations[i].z = 0.0;
+
+        for (int j = 0; j < bodies; j++) {
+            if (i != j) {
+                double dx = positions[j].x - positions[i].x;
+                double dy = positions[j].y - positions[i].y;
+                double dz = positions[j].z - positions[i].z;
+
+                double dist = sqrt(dx * dx + dy * dy + dz * dz) + epsilon;
+
+                double invDistCubed = 1.0 / (dist * dist * dist);
+
+                double scalar = GravConstant * masses[j] * invDistCubed;
+
+                accelerations[i].x += scalar * dx;
+                accelerations[i].y += scalar * dy;
+                accelerations[i].z += scalar * dz;
+            }
+        }
+    }
 }
 
+
 void computeVelocities() {
-	for (int i = 0; i < bodies; i++)
-		velocities[i] = addVectors(velocities[i], scaleVector(dt, accelerations[i])); //semi-implicit euler
+	for (int i = 0; i < bodies; i++) {
+		velocities[i].x = velocities[i].x + dt * accelerations[i].x;
+                velocities[i].y = velocities[i].y + dt * accelerations[i].y;
+                velocities[i].z = velocities[i].z + dt * accelerations[i].z;
+                }
 }
 
 void computePositions() {
-	for (int i = 0; i < bodies; i++)
-		positions[i] = addVectors(positions[i], scaleVector(dt, velocities[i])); //semi-implicit euler
+	for (int i = 0; i < bodies; i++) {
+		positions[i].x = positions[i].x + dt * velocities[i].x;
+		positions[i].y = positions[i].y + dt * velocities[i].y;
+		positions[i].z = positions[i].z + dt * velocities[i].z;
+		}
 }
 
 
-void simulate(){
-	computeAccelerations();
-	computeVelocities(dt);
-	computePositions(dt);
+void simulate(){ //195*bodies^2+164*bodies
+	computeAccelerations();//(179+16)*bodies^2
+	computeVelocities(dt); //(66+16)*bodies
+	computePositions(dt); //(66+16)*bodies
 }
 
 void logPositions(FILE *fp) {
@@ -115,12 +112,12 @@ int main(int argC,char* argV[])
 {
 	int i,j;
 	FILE *output = fopen("trajectory_serial.csv", "w");
-    	initiateSystem(bodies);
+    	initiateSystem(bodies); //286*bodies
     	logPositions(output);
       	printf("Body   :     x              y               z           |       vx              vy              vz   ");
-		for(i=0;i<N;i++){
+		for(i=0;i<N;i++){//195*T*bodies^2+164*T*bodies
 			printf("\nCycle %d\n",i+1);
-			simulate(dt);
+			simulate(dt); //195*bodies^2+164*bodies
 			logPositions(output);
 			/**for(j=0;j<bodies;j++)
 				printf("Body %d : %lf\t%f\t%lf\t|\t%lf\t%lf\t%lf\n",j+1,positions[j].x,positions[j].y,positions[j].z,velocities[j].x,velocities[j].y,velocities[j].z);**/
